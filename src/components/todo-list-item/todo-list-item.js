e94bdeee7f3131f55e63d73f8772d8b1d15649aa
PolonevichA <andrey.polonevich0209@gmail.com>
2021-07-04 15:47:54 +0300

    папки, выполненное, важное
    1. Разложил по папкам
    2. Возможность помечать как важное и отменять по клику на кнопку
    3. Возможность помечать как выполненное и отменять по клику на текст

---
+++ src/components/todo-list-item/todo-list-item.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+
+import './todo-list-item.css';
+
+export default class TodoListItem extends Component{
+  
+  constructor() {
+    super()
+
+    this.state = {
+      done: false,
+      important: false,
+    }
+
+    this.onLabelClick = () => {
+      this.setState(({ done }) => {
+        return {
+          done: !done,
+        }
+      })
+    }
+    this.onMarkImpotant = () => {
+      this.setState(({important}) => {
+        return {
+          important: !important,
+        }
+      })
+    }
+  }
+
+  render() {
+
+    const { label } = this.props;
+    const { done, important } = this.state;
+
+    let className = 'todo-list-item';
+    if(done){
+      className += ' done';
+    }
+    if(important){
+      className += ' important'
+    }
+
+
+  
+    return (
+      <span className={className}>
+        <span
+          className="todo-list-item-label"
+          onClick = { this.onLabelClick }>
+          {label}
+        </span>
+  
+        <button type="button"
+                className="btn btn-outline-success btn-sm float-right" 
+                onClick ={ this.onMarkImpotant }>
+          <i className="fa fa-exclamation" />
+        </button>
+  
+        <button type="button"
+                className="btn btn-outline-danger btn-sm float-right">
+          <i className="fa fa-trash-o" />
+        </button>
+      </span>
+    );
+  }
+}
+
--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import SearchPanel from './components/search-panel';
-import AppHeader from './components/app-header';
-import TodoList from './components/todo-list';
+import App from './components/app';
+import './index.css';
 
 
 
-const App = () => {
-  return (
-    <div>
-      <AppHeader />
-      <SearchPanel />
-      <TodoList />
-    </div>
-  );
-};
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />,
+  document.getElementById('root'));
